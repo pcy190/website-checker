255126e2c0657a1e9f3fca0848e3e35b22b0e67c
HAPPY <pcy190@126.com>
2020-02-02 21:27:13 +0800

    first version

---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const signale = require('signale')
+const moment = require('moment')
+const { mustExist, checkWebsiteExist } = require('./utils')
+const Octokit = require('@octokit/rest')
+
+const {
+  GIST_ID: gistID,
+  GH_TOKEN: githubToken,
+  LISTENED_WEBSITES: targets
+} = process.env
+
+const octokit = new Octokit({ auth: `token ${githubToken}` })
+
+;[
+  [gistID, 'GIST_ID'],
+  [githubToken, 'GH_TOKEN'],
+  [targets, 'LISTEN_WEBSITE']
+].forEach(([v, n]) => mustExist(v, n))
+
+;(async () => {
+  signale.pending('Fetching gists...')
+  const gist = await octokit.gists.get({ gist_id: gistID })
+    .catch(error => { throw new Error(`Unable to get gist\n${error}`) })
+  const sites = targets.split(',')
+  const lines = []
+  for (const key in sites) {
+    const site = sites[key]
+    signale.watch(`Checking site: ${site}...`)
+    const exist = await checkWebsiteExist(site)
+    if (exist) {
+      lines.push(`${site} works properly...`)
+    } else {
+      lines.push(`${site} has shut down!!!`)
+    }
+  }
+  lines.push(`Update at ${moment().format('dddd, MMMM Do YYYY, k:mm:ss Z')}`)
+  try {
+    const fileName = Object.keys(gist.data.files)[0]
+    signale.pending('Upload new gist content...')
+    await octokit.gists.update({
+      gist_id: gistID,
+      files: {
+        [fileName]: {
+          fileName: 'Did following site(s) shut down?',
+          content: lines.join('\n')
+        }
+      }
+    })
+  } catch (error) {
+    throw Error('Cannot update gist')
+  } finally {
+    signale.success('Done!')
+  }
+})()
---
+++ utils/index.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+const signale = require('signale')
+
+exports.mustExist = (value, name = value) => {
+  if (!value) {
+    throw new TypeError(`Environment variable: '${name}' must be set.`)
+  }
+}
+
+
+// checker
+exports.checkWebsiteExist = async (site) => {
+  if (typeof site !== 'string') {
+    throw new TypeError('site name must be a string.')
+  }
+  let exist = false
+  for (let i = 0; i < 3; i++) {
+    // tip: check 3 times
+    exist |= await axios.get(site).then(response => response.status !== 404)
+      .catch(error => signale.error(error))
+  }
+  return exist
+}
